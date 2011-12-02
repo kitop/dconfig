92b9fbf74f7e2817c4095c7e0bac073309b56196
Maximiliano Javier Gonzalez <maxjgon@gmail.com>
2011-12-02 15:48:59 -0300

    Add code to load yml config and test yml file

--- lib/dconfig.rb
+++ lib/dconfig.rb
@@ -5,9 +5,33 @@ DCONFIG_PATH = "#{File.dirname(__FILE__)}/dconfig"
 require "#{DCONFIG_PATH}/railtie.rb"
 
 module Dconfig
+  class UndefinedDconfig < StandardError; end
   class << self
     def setup!
-
+      yml = get_yml
+      create_dconfig_class(yml)
     end
-  end
+
+    def get_yml
+      "#{Rails.root.to_s}/config/dconfig.yml"
+    end
+
+    def load_yml(yml_file)
+      erb = ERB.new(File.read(yml_file)).result
+      hash = YAML.load(erb).to_hash[Rails.env]
+      klass = Object.const_set('DConfig', Class.new)
+      hash.each do |key,value|
+        klass.define_singleton_method(key){ value }
+      end
+      klass.class_eval do
+        def self.method_missing(method_id,*args)
+          raise UndefinedDconfig, "#{method_id} is not defined in #{self.to_s}"
+        end
+      end
+    end
+
+    def create_dconfig_class(yml_file)
+      hash = load_yml(yml_file)
+    end
+  end # class << self
 end
