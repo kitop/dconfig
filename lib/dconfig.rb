ebd7fa632aa056cf6e1ae46c052912ef84ca5e09
Maximiliano Javier Gonzalez <maxjgon@gmail.com>
2011-12-02 16:54:50 -0300

    Get all values from redis

--- lib/dconfig.rb
+++ lib/dconfig.rb
@@ -11,23 +11,49 @@ module Dconfig
     def setup!
       yml = get_yml
       redis = get_redis
-      create_dconfig_class(yml)
+      create_dconfig_class(yml, redis)
     end
 
     def get_yml
       "#{Rails.root.to_s}/config/dconfig.yml"
     end
 
+    def app_name
+      Rails.application.class.to_s.split("::").first
+    end
+
     def get_redis
       config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
-      Redis.new(:host => config['host'], :port => config['port'])
+      redis = Redis.new(:host => config['host'], :port => config['port'])
+      Redis::Namespace.new(app_name, :redis => redis)
     end
 
     def load_yml(yml_file)
       erb = ERB.new(File.read(yml_file)).result
-      hash = YAML.load(erb).to_hash[Rails.env]
+      YAML.load(erb).to_hash[Rails.env]
+    end
+
+    def load_from_redis(redis)
+      redis.hgetall('rdconfig')
+    end
+
+    def add_missed_fields_to_redis(redis, hash_yml)
+      hash_yml.each do |field, value|
+        redis.hsetnx 'dconfig', field, value
+      end
+    end
+
+    def create_dconfig_class(yml_file, redis)
+      hash_yml = load_yml(yml_file)
+      hash_redis = load_from_redis(redis)
+
+      add_missed_fields_to_redis(redis, hash_yml)
+
+      hash_redis = load_from_redis(redis)
+
       klass = Object.const_set('DConfig', Class.new)
-      hash.each do |key,value|
+
+      hash_redis.each do |key,value|
         klass.define_singleton_method(key){ value }
       end
       klass.class_eval do
@@ -35,10 +61,6 @@ module Dconfig
           raise UndefinedDconfig, "#{method_id} is not defined in #{self.to_s}"
         end
       end
-    end
-
-    def create_dconfig_class(yml_file)
-      hash = load_yml(yml_file)
     end
   end # class << self
 end
